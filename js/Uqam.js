2894c10a75981a73adc2dcb3bbd215bfae96f177
Nelson Moller <moller_clara.nelson@uqam.ca>
2016-12-19 14:12:32 -0500

    Refactoring pour éviter duplications.

--- js/Uqam.js
+++ js/Uqam.js
@@ -25,7 +25,36 @@ Uqam = {
             second: parseInt(temp[2])
         }
 
+    },
+
+    prepareD3Data: function(d){
+        d.time = this.dateParser(d.time);
+        hour = this.splitHourMinutesSecs(d.time[3])
+        d.time = new Date(d.time[5],
+                this.convertMonthToJSInt(d.time[1]),
+                d.time[2],
+                hour.hour, hour.minute, hour.second);
+        d.total = parseFloat(d.total.slice(2,7));
+    },
+    
+    addGraphAxes: function (svg, data) {
+        // Add the valueline path.
+        svg.append("path")
+            .datum(data)
+            .attr("class", "line")
+            .attr("d", valueline);
+        // Add the X Axis
+        svg.append("g")
+            .attr("class", "axis axis--x")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
+        // Add the Y Axis
+        svg.append("g")
+            .attr("class", "axis axis--y")
+            .call(yAxis);
+        //
     }
+
 };
 // Tests de la clase Uqam
 //var dateArray = Uqam.dateParser('Tue Dec 13 22:08:09 EST 2016');
